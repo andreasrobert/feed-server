44de8b62039a4ab580146d236d63f6621cb1d606
andreasrobert <andreasrobb23@gmail.com>
2021-08-22 00:32:12 +0700

    debug5

--- src/verifyToken.ts
+++ src/verifyToken.ts
@@ -4,7 +4,6 @@ import cookie from "cookie";
 
 export default function (req:any, res:any, next:any) {
     console.log(req.headers)
-    res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
